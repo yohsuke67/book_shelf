bc60391ee6bd195753e2665982e6538c61a23536
yohsuke67 <yousuke67@gmail.com>
2019-05-03 19:30:53 +0900

    パーシャルフォーム作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,14 +2,24 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
   end
+  
   def new
     @book = Book.new
   end
+  
   def create
     @book = Book.new (book_params)
-    @book.save
-    redirect_to @book, notice:”書籍を登録しました”
+    if @book.save
+      redirect_to @book, notice: "書籍を登録しました。"
+    else
+      render :new
+    end
+    end
+  
+  def show
+    @book = Book.find(params[:id])
   end
+
   private
 
   def book_params

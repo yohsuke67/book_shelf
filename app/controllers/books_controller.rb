f0269e348a64f89a7cc99d258323de1ef9478e3b
yohsuke67 <yousuke67@gmail.com>
2019-05-04 14:05:44 +0900

    カテゴリーモデル追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
-  
+
   def index
     @books = Book.all
   end
@@ -41,7 +41,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :price, :publish_date,:description, :new_image)
+    params.require(:book).permit(:title, :price, :publish_date,:description, :new_image,:category_id)
   end
 
   def set_book

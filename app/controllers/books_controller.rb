e479f423c5c822784e135e14f397725c7b4b4cbe
yohsuke67 <yousuke67@gmail.com>
2019-05-04 09:38:23 +0900

    画像アップロード導入

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,12 +9,12 @@ class BooksController < ApplicationController
   
   def create
     @book = Book.new (book_params)
-    if @book.save
-      redirect_to @book, notice: "書籍を登録しました。"
-    else
-      render :new
-    end
-    end
+  if @book.save
+     redirect_to @book, notice: "書籍を登録しました。"
+   else
+     render :new
+   end
+  end
   
   def show
     @book = Book.find(params[:id])
@@ -23,7 +23,9 @@ class BooksController < ApplicationController
   private
 
   def book_params
+    
     params.require(:book).permit(:title, :price, :publish_date, :description)
+    params.require(:book).permit(:title, :price, :publish_date,:description, :new_image)
   end
 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,8 @@
 class Book < ApplicationRecord
+    has_one_attached :image
+    attribute :new_image
+
+    
 validates :title, presence: true,
 length: {maximum:50}
 validates :price, presence: true,
@@ -9,5 +13,9 @@ numericality:{
 validates :publish_date, presence: true
 validates :description, presence: true,
 length:{maximum: 1000}
+
+before_save do
+self.image = new_image if new_image
+end
 
 end

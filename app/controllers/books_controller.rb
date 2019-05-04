b9ffe9fa1436d51749c5e155b08a668fa7aae3f7
yohsuke67 <yousuke67@gmail.com>
2019-05-04 17:45:20 +0900

    レビュー投稿機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,7 @@ class BooksController < ApplicationController
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   def index
-    @books = Book.page(params[:page]).per(4)
+    @books = Book.with_attached_image.page(params[:page]).per(4)
   end
   
   def new
